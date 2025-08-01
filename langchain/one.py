7b7ae9060614d00f80243facbd25ebb169b8bed0
YashChaudhary16 <yashchaudhary.yc16@gmail.com>
2025-08-01 10:13:19 -0400

    Added new dependencies for LangChain and created initial Streamlit app and text-to-SQL functionality.

---
+++ langchain/one.py
@@ -0,0 +1,22 @@
+#load env variables
+import os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='.env', override=True)
+
+
+# import libraries
+import streamlit as st
+
+# streamlit app
+
+st.title("Text to SQL")
+
+
+
+
+
+
+
+
+
+
